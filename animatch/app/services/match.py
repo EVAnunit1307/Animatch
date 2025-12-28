8939d8b46cb15c3115277598f698c0b7ccb28edd
Evan Liem <evan.sl1307@gmail.com>
2025-12-28 17:26:20 -0500

    finished sort best match

--- animatch/app/services/match.py
+++ animatch/app/services/match.py
@@ -14,3 +14,28 @@ def distance(user_vector, char_vector):
         diffrence = user_vector[i] - char_vector[i]
         total += diffrence * diffrence
     return math.sqrt(total)
+
+def match_characters (user_features, top_k=3): #gets top 3 
+    char = load_characters()
+    scored = []
+
+    for c in char:
+        d = distance(user_features, c["vector"])
+        sim = 1.0 / (1.0/d)
+        scored.append((sim,c)) #ppends similarioty score and character 
+
+    scored.sort(key=lambda x: x[0], reverse=True) #sorts by the first thing in tuple, from decending 
+
+    results = []
+    for sim, c in scored[:top_k]:#goes to scores takes top 3 
+        results.append({
+            "id": c["id"],
+            "name": c["name"],
+            "series": c["series"],
+            "similarity": round(sim, 4),
+            "vector": c["vector"]
+        })
+
+    return results
+
+
