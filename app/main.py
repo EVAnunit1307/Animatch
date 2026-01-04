7eec90b11283369a760070c63aa03b02169642da
Evan Liem <evan.sl1307@gmail.com>
2026-01-04 13:09:30 -0500

    adding featured series

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
-"""
-Lightweight adapter so you can run:
-    uvicorn app.main:app --reload
 
-It re-exports the real FastAPI instance from animatch.app.main.
-"""
 from animatch.app.main import app  # noqa: F401
 
